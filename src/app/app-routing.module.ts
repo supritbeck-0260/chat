0e6cc2e6e34834a6744121ce037f628ee4702643
Suprit <supritbeck@gmail.com>
2021-05-09 23:14:31 +0530

    routing added and home page initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [   
+  {
+    path:'chat',
+    component: ChatRoomComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ChatRoomComponent } from './chat-room/chat-room.component';
 import { MessagesComponent } from './messages/messages.component';
 import { InputComponent } from './input/input.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     ChatRoomComponent,
     MessagesComponent,
     InputComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
