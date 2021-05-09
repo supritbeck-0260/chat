2b11da1c2fe49f4d3e3e01818d11c89339f7f921
Suprit <supritbeck@gmail.com>
2021-05-09 23:26:40 +0530

    home page template created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [   
   {
     path:'chat',
     component: ChatRoomComponent
+  },
+  {
+    path:'',
+    component: HomeComponent
   },
 ];
 
