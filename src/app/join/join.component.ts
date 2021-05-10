78fa0ab1ef9ec73fb3ff8f721c052a26874f431d
Suprit <supritbeck@gmail.com>
2021-05-10 23:21:37 +0530

    functionality added in join component

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
+interface User{
+  room:string;
+  name:string;
+}
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.css']
 })
+
 export class JoinComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router : Router) { }
+  user:User={
+    room:'',
+    name:''
+  };
+  joinHandler(){
+    if(this.user.room && this.user.name) this.router.navigate(['chat']);
+  }
   ngOnInit() {
   }
 
