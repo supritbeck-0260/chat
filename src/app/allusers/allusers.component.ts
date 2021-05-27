cdf4404395674b77de877d3dc4813678d4adb6f9
suprit beck <supritbeck@gmail.com>
2021-05-27 20:21:48 +0530

    show all users on all users component

--- src/app/allusers/allusers.component.ts
+++ src/app/allusers/allusers.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
-
+interface User{
+  userName:String,
+  email:String,
+  date?:String
+}
 @Component({
   selector: 'app-allusers',
   templateUrl: './allusers.component.html',
   styleUrls: ['./allusers.component.css']
 })
 export class AllusersComponent implements OnInit {
-
+  users:User;
   constructor(private http:HttpService) { }
 
   ngOnInit() {
-    this.http.getusers().subscribe(data=>console.log(data));
+    this.http.getusers().subscribe((data:User)=>{
+      if(data) this.users = data;
+    });
   }
 
 }
