9ea6078a82f44027f925a9c4904a5223b766996e
suprit beck <supritbeck@gmail.com>
2021-06-05 14:42:47 +0530

    dynamic port

--- server/index.js
+++ server/index.js
@@ -12,5 +12,6 @@ app.use('/api',router);
 app.use(cors());
 app.use(cookieParser());
 const liveChat = require('./socket');
-const server = app.listen(5000,()=>console.log('server is running...'));
+const port = process.env.PORT || 5000;
+const server = app.listen(port,()=>console.log('server is running...'));
 liveChat(server);
